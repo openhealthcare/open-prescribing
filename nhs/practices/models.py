cf08543d4ddf22fc86607d75fdc8ad4ec9be2298
David Miller <david@deadpansincerity.com>
2013-02-25 16:31:31 +0000

    Add dispensing field to practices

--- nhs/practices/models.py
+++ nhs/practices/models.py
@@ -6,13 +6,16 @@ from nhs.ccgs.models import CCG
 from mapit.models import Postcode
 
 class Practice(geo_models.Model):
-    practice = models.CharField(blank=True, max_length=100, primary_key=True)
-    name = models.CharField(blank=True, max_length=255)
-    postcode = models.CharField(blank=True, max_length=10)
-    pc = models.ForeignKey(Postcode, null=True, blank=True)
-    imd = models.FloatField(blank=True, null=True)
-    address = models.TextField(blank=True)
-    ccg = models.ForeignKey(CCG, null=True)
+    practice   = models.CharField(blank=True, max_length=100, primary_key=True)
+    name       = models.CharField(blank=True, max_length=255)
+    postcode   = models.CharField(blank=True, max_length=10)
+    pc         = models.ForeignKey(Postcode, null=True, blank=True)
+    imd        = models.FloatField(blank=True, null=True)
+    address    = models.TextField(blank=True)
+    ccg        = models.ForeignKey(CCG, null=True)
+    dispensing = models.NullBooleanField(blank=True, null=True)
+
+
     objects = geo_models.GeoManager()
 
     def __unicode__(self):
