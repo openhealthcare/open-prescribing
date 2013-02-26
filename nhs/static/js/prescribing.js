3b548b987160482812c54843cce4655f8622d0f9
David Miller <david@deadpansincerity.com>
2013-02-26 16:21:05 +0000

    Bump leaflet attribution. Give copious thanks in a colophon.

--- nhs/static/js/prescribing.js
+++ nhs/static/js/prescribing.js
@@ -77,12 +77,17 @@
         },
 
         initialize: function(identifier){
-            var map = L.map(identifier).setView([53.0, -1.5], 7);
-            var cloudmade = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
-                attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-                key: 'BC9A493B41014CAABB98F0471D759707',
-                styleId: 22677
-            }).addTo(map);
+            var map = L.map(
+                identifier,
+                {
+                    attributionControl: false
+                }
+            ).setView([53.0, -1.5], 7);
+            var cloudmade = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png',
+                                        {
+                                            key: 'BC9A493B41014CAABB98F0471D759707',
+                                            styleId: 22677
+                                        }).addTo(map);
             return map;
         },
 
