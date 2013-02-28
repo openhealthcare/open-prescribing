6a6f76a5e1340436da5e48dd5c8eaa1ef30ed4e1
David Miller <david@deadpansincerity.com>
2013-02-28 18:27:42 +0000

    Create an example client application that displays the sustainable healthcare HFC inhaler graphs. Closes #9

--- nhs/static/js/clients/inhaler.js
+++ nhs/static/js/clients/inhaler.js
@@ -1,75 +1,33 @@
+//
+// inhaler.js
 
-var inhalerCodes = {
-    "naughty": [
-        "0301011R0BMAAAP",
-        "0301011R0BJABAF",
-        "0301011E0BDAAAD",
-        "0302000N0AAAXAX",
-        "0302000N0AABFBF",
-        "0302000N0AABGBG",
-        "0302000N0AAAYAY",
-        "0302000N0AABEBE",
-        "0302000N0AAAZAZ",
-        "0302000N0BDABBK",
-        "0302000N0BDACBL",
-        "0302000C0BQAABX",
-        "0301011R0BIAGBU",
-        "0301011R0BIAAAA",
-        "0301011R0BIAHAP",
-        "0301011R0BIAFAP",
-        "0301011R0BXABCC",
-        "0301011R0BXAACB",
-        "0301011U0AAACAC",
-        "0301011U0AAAHAH",
-        "0301011U0AAAEAE",
-        "0301011U0AAABAB",
-        "0301011U0AAAAAA"
-    ],
-    "nice": [
-        "0302000K0AAALAL",
-        "0302000K0AAAMAM",
-        "0302000K0AAAUAU",
-        "0301011E0BBAAAA",
-        "0301011E0AAADAD",
-        "0301011E0AAACAC",
-        "0301011E0AAABAB",
-        "0301011E0AAAAAA",
-        "0301011X0BBAAAA",
-        "0301011X0BBABAB",
-        "0301011R0BTAABX",
-        "0301040R0AAAAAA",
-        "0301011R0AAAXAX",
-        "0301011R0AABDBD",
-        "0301011R0AABBBB",
-        "0301011R0AAAPAP",
-        "0301011R0AAADAD",
-        "0301011R0AABUBU",
-        "0301011R0AABZBZ",
-        "0301011R0AACCCC",
-        "0301011R0AACBCB",
-        "0301011R0AABYBY",
-        "0301011R0AACACA",
-        "0301011R0AAAQAQ",
-        "0301011R0AAAFAF",
-        "0301011R0AAAVAV",
-        "0301011R0AAAWAW",
-        "0301011U0BBACAC"
-    ]
+// Open Prescribing API Client example application
+//
+//
+// The Inhaler app shows the relative ratios of prescribing
+// between two buckets of inhalers that the researchers are
+// interested in for reasons documented on the page.
+//
+// This example is deliberately minimal in order to show a
+// basic use case on which to build.
 
-$(document).ready(function(){
+// Coding begins
+(function(context, namespace){
 
-    Scrip.practice.bucket_compare(
-        naughty: inhalerCodes.naughty,
-        nice: inhalerCodes.nice,
-        map: map
-    );
+    // Set up the Marionette app & the prescribing.js instance.
+    var Inhaler = context[namespace] = new Backbone.Marionette.Application();
+    var OP = new Scrip({api_host: window.location.host});
 
-    Scrip.ccg.bucket_compare({
-        naughty: inhalerCodes.naughty,
-        nice: inhalerCodes.nice,
-        map: map
-
+    Inhaler.addRegions({container: '#inhaler-map'});
+    Inhaler.addInitializer(function(options){
+        var results = new OP.views.ResultLayout();
+        Inhaler.container.show(results);
+        var mapview = OP.maps.bucket({
+            bucket1: metered,
+            bucket2: powdered,
+            practices: true
+        });
+        OP.trigger('results:new_view', mapview);
     });
-})
 
-//{"A81016": {"group1": {"items": 1, "proportion": 100}, "group2": {"items": 0, "proportion": 0}}}
+})(this.window||exports, "Inhaler")
