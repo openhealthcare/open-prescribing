1bcf498d97ce2a760d299e2aab36a21807af5f81
David Miller <David>
2013-03-27 15:51:18 +0000

    Update raw urls for single drugs

--- nhs/raw/urls.py
+++ nhs/raw/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns(
     url(r'/ratio/(?P<bucket1>[0-9A-Z]+)/(?P<bucket2>[0-9A-Z]+)/ratio.zip$',
         Ratio.as_view(), name='rawcompare'),
 
-    url(r'/drug/percapitamap/ccg/(?P<bnf_code>[0-9A-Z]+)/percap.zip$',
+
+    url(r'/drug/(?P<bnf_code>[0-9A-Z]+)/percap.zip$',
         Drug.as_view(), name='rawdrug'),
     )
